cd26fa058c7597798619980dc9cae0c4f586189b
Linda Grealish <lindagrealish@hotmail.com>
2024-02-12 11:07:56 +0000

    Fix code to convert wind direction deg to text

--- assignments/currentweather.py
+++ assignments/currentweather.py
@@ -17,6 +17,17 @@ url2 = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&cur
 
 response2 = requests.get(url2).json()
 
-winddir = response['current']['wind_direction_10m']
+wind_direction_degrees = response2['current']['wind_direction_10m']
 
-print ("The wind direction is", winddir)
+# Take the wind direction in degrees and convert it to text.
+# https://stackoverflow.com/questions/7490660/converting-wind-direction-in-angles-to-text-words
+def degrees_to_text(wind_direction_degrees):
+    directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+
+    index = round(wind_direction_degrees / 22.5) % 16
+    return directions[index]
+
+
+wind_direction_text = degrees_to_text(wind_direction_degrees)
+print("The wind is coming from a", wind_direction_text, "direction.")
+
