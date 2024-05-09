92b61b2d7c2cd9b7ec2fb4efbff1f257dfa88ea0
Linda Grealish <lindagrealish@hotmail.com>
2024-05-09 10:14:00 +0100

    Commit assignment 4 content

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # WSAA-coursework
-Assignments and Labs for Web Services and Applications
+Assignments and Labs for Web Services and Applications Module 2024 at ATU as part of the Higher Diploma in Computing and Data Analytics
+
+## Repository contents:
+
+* .gitignore
+
+* License
+
+* assignments
+
+    * currentweather.py
+
+        * currentwinddir10m.json
+
+        * currenttemperature2m.json
+
+    * assignment03-cso.py
+
+        * cso.json
+
+    * assignment04-github.py
+
+
+
+* labs
+
+* README.md
+
+
+## Technologies Used
+
+* Visual Studio Code - version 1.84.2
+* Python 3.11.4
+
+
+
+## Software required to run the programs
+
+* [Anaconda](https://www.anaconda.com/download)
+
+* [Visual Studio Code](https://code.visualstudio.com/) 
+
+
+
+
+
+
--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -3,3 +3,30 @@
 
 # Author: Linda Grealish
 
+from github import Github
+import requests
+from config import config as cfg
+
+apikey = cfg["githubkey"] # apikey contained in config.py
+
+g = Github(apikey,verify = False) 
+
+repo = g.get_repo("lgrealish/aprivateone") # url of repo
+
+fileInfo = repo.get_contents("file.txt") # file that I want to update
+urlOfFile = fileInfo.download_url
+print (urlOfFile)
+
+response = requests.get(urlOfFile,verify = False)
+contentOfFile = response.text
+print (contentOfFile)
+
+
+# replacing Andrew with Linda in file.txt
+string = contentOfFile
+new_string = string.replace("Andrew", "Linda")
+print(new_string)
+
+# update file in github with commit message
+gitHubResponse=repo.update_file(fileInfo.path,"amend Andrew to Linda and add new",newContents,fileInfo.sha)
+print (gitHubResponse)
